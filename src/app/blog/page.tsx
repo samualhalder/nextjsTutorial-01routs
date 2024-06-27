ab625c987832ff91514e0cb3b09987b9e6ce6e08
samualhalder <samual.halder2020@gmail.com>
2024-06-27 21:17:03 +0530

    error handling

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,8 +1,19 @@
+
 import React from 'react'
 
 function Blog() {
+  const generateError=()=>{
+    const random=Math.round((Math.random()*2));
+    if(random==1){
+      throw  new Error("Connection lost")
+    }
+  }
+  generateError()
   return (
-    <div>Blog</div>
+    <div>
+      Blog
+     
+    </div>
   )
 }
 
